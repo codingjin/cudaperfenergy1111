e900ed792eb33868d3f988ec8be16f9b8bf123d7
codingjin <475166289@qq.com>
2025-11-11 03:39:13 -0700

    dir

--- run.sh
+++ run.sh
@@ -4,24 +4,24 @@
 make
 
 # Create output folders
-mkdir -p smem db unrolling vec
+mkdir -p smemdir dbdir unrollingdir vecdir
 
 # Run smem benchmarks
-./smem_perf 4096 4096 4096 2>&1 | tee smem/perf
-./smem_energy 4096 4096 4096 2>&1 | tee smem/energy
+./smem_perf 4096 4096 4096 2>&1 | tee smemdir/perf
+./smem_energy 4096 4096 4096 2>&1 | tee smemdir/energy
 
 # Run db benchmarks
-./db_perf 4096 4096 4096 2>&1 | tee db/perf
-./db_energy 4096 4096 4096 2>&1 | tee db/energy
+./db_perf 4096 4096 4096 2>&1 | tee dbdir/perf
+./db_energy 4096 4096 4096 2>&1 | tee dbdir/energy
 
 # Run unrolling benchmarks
-./i2_perf 4096 4096 4096 2>&1 | tee unrolling/i2_perf
-./i2_energy 4096 4096 4096 2>&1 | tee unrolling/i2_energy
-./i4_perf 4096 4096 4096 2>&1 | tee unrolling/i4_perf
-./i4_energy 4096 4096 4096 2>&1 | tee unrolling/i4_energy
+./i2_perf 4096 4096 4096 2>&1 | tee unrollingdir/i2_perf
+./i2_energy 4096 4096 4096 2>&1 | tee unrollingdir/i2_energy
+./i4_perf 4096 4096 4096 2>&1 | tee unrollingdir/i4_perf
+./i4_energy 4096 4096 4096 2>&1 | tee unrollingdir/i4_energy
 
 # Run vectorization benchmarks
-./vec2_perf 4096 4096 4096 2>&1 | tee vec/vec2_perf
-./vec2_energy 4096 4096 4096 2>&1 | tee vec/vec2_energy
-./vec4_perf 4096 4096 4096 2>&1 | tee vec/vec4_perf
-./vec4_energy 4096 4096 4096 2>&1 | tee vec/vec4_energy
+./vec2_perf 4096 4096 4096 2>&1 | tee vecdir/vec2_perf
+./vec2_energy 4096 4096 4096 2>&1 | tee vecdir/vec2_energy
+./vec4_perf 4096 4096 4096 2>&1 | tee vecdir/vec4_perf
+./vec4_energy 4096 4096 4096 2>&1 | tee vecdir/vec4_energy
